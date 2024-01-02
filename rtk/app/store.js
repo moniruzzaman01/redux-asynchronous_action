63811817d6ad8cf4ecf6d27a48ec56bb73d87636
moniruzzaman01 <moniruzzamanshakib04@gmail.com>
2024-01-03 01:59:48 +0600

    redux logger added

--- rtk/app/store.js
+++ rtk/app/store.js
@@ -1,11 +1,14 @@
 const { configureStore } = require("@reduxjs/toolkit");
 const todosReducer = require("../features/todos/todosSlice");
+const reduxLogger = require("redux-logger");
+const logger = reduxLogger.createLogger();
 
 //store
 const store = configureStore({
   reducer: {
     todos: todosReducer,
   },
+  middleware: (defaultMiddlewares) => defaultMiddlewares().concat(logger),
 });
 
 module.exports = store;
